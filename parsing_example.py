d148acac6be94765cf150f9b14df4cc707ebe194
Sunshine Abbott <sunshine.abbott@gmail.com>
2019-09-01 14:37:33 +0100

    made initial spider class

--- parsing_example.py
+++ parsing_example.py
@@ -1,3 +1,21 @@
+from lxml import html, etree
 import requests
 
 # Parsing involves taking the raw data that we get from the website and parse it into a structured way
+
+# Get an example page, of the Audio category from Indiegogo.com
+page = requests.get("https://www.indiegogo.com/explore/audio?project_type=campaign&project_timing=all&sort=trending")
+print (page.content)
+
+tree = html.fromstring(page.content)
+print (tree)
+
+all_nodes = tree.xpath("//*")
+
+# Select only the hyperlink nodes
+hyperlink_nodes = tree.xpath("//*/a")
+
+# Iterate through the node links
+for node in hyperlink_nodes:
+    print (node.values())
+
--- scraping_example.py
+++ scraping_example.py
@@ -5,3 +5,5 @@ page = requests.get("https://www.indiegogo.com/projects/pocketdrum-the-most-port
 
 # Print the contents that we get from the above web page (the raw data from the URL above)
 print (page.content)
+
+# We have the data from the above web page, now we would need to parse that data to get what we want
