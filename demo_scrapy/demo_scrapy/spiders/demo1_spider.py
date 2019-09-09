75286c7ae68a4eff3ed36d4cfb70ab2e1955b032
Sunshine Abbott <sunshine.abbott@gmail.com>
2019-09-09 10:05:49 +0100

    added code to spider

--- demo_scrapy/demo_scrapy/spiders/demo1_spider.py
+++ demo_scrapy/demo_scrapy/spiders/demo1_spider.py
@@ -2,6 +2,9 @@ import scrapy
 
 class CrowdfundSpider(scrapy.Spider): # Use the Spider class provided by Scrapy and make a subclass out of it called CrowdfundSpider
     name = "indiegogo_audio" # This is the name of the spider, which will be used to run the spider when `scrapy crawl name_of_spider` is used
-    start_urls = ["https://www.indiegogo.com/projects/pamu-slide-born-for-music-never-fall-out#/"]
+    start_urls = ["https://www.indiegogo.com/explore/audio?project_type=campaign&project_timing=all&sort=trending"]
 
-
+def parse(self, response):
+    campaign_SELECTOR = '.discoverableCard'
+    for campaign in response.css(campaign_SELECTOR):
+        pass
