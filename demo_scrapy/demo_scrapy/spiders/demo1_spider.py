bc5eb39e9cbdb461ddf9df6e704417d46521285e
Sunshine Abbott <sunshine.abbott@gmail.com>
2019-09-16 11:37:29 +0100

    updated spider

--- demo_scrapy/demo_scrapy/spiders/demo1_spider.py
+++ demo_scrapy/demo_scrapy/spiders/demo1_spider.py
@@ -1,10 +1,15 @@
 import scrapy
 
 class CrowdfundSpider(scrapy.Spider): # Use the Spider class provided by Scrapy and make a subclass out of it called CrowdfundSpider
-    name = "indiegogo_audio" # This is the name of the spider, which will be used to run the spider when `scrapy crawl name_of_spider` is used
-    start_urls = ["https://www.indiegogo.com/explore/audio?project_type=campaign&project_timing=all&sort=trending"]
+    name = "fundrazr_accidents" # This is the name of the spider, which will be used to run the spider when `scrapy crawl name_of_spider` is used
+    start_urls = ["https://fundrazr.com/find?category=Accidents"]
 
 def parse(self, response):
-    campaign_SELECTOR = '.discoverableCard'
-    for campaign in response.css(campaign_SELECTOR):
-        pass
+    campaign_titles = response.xpath('//h2//a[starts-with(@href, "//fund")]/text()').extract() 
+    urls = []
+    for href in response.xpath('//h2//a//@href'):
+        url = "https:" + href.extract()
+        urls.append(url)
+
+    yield {'Campaign titles': campaign_titles, 'URL': urls}
+    
