50d19c9919abfe77d8a5b87f346418b5ba70fd23
Sunshine Abbott <sunshine.abbott@gmail.com>
2019-09-19 14:12:10 +0100

    refactored

--- demo_scrapy/demo_scrapy/spiders/demo1_spider.py
+++ demo_scrapy/demo_scrapy/spiders/demo1_spider.py
@@ -1,6 +1,5 @@
 import scrapy
 
-
 class CrowdfundSpider(scrapy.Spider):
     name = "fundrazr_campaigns"
     allowed_domains = ["fundrazr.com"]
@@ -9,14 +8,17 @@ class CrowdfundSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        category = response.xpath('//li[@class="active"]//a[starts-with(@href, "https://fundrazr.com/find?category=")]/text()').extract()
         widget_tall = response.xpath('//*[@class="widget tall"]')
         for campaign in widget_tall:
             campaign_message = campaign.xpath('.//@data-message').extract() # Use extract_first() to get only the first campaign
             owner_name = campaign.xpath('.//@data-ownername').extract()
-            
-            yield{'Campaign message': campaign_message, 'Owner name' : owner_name}
+            location = campaign.xpath('.//p[@class="location"]/text()').extract()
+            campaign_title = campaign.xpath('.//h2//a[starts-with(@href, "//fund")]/text()').extract()
+
+            yield{'Category': category, 'Campaign title': campaign_title, 'Owner name' : owner_name, 'Location': location, 'Campaign description': campaign_message}
             
         next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
         absolute_next_page_url = response.urljoin(next_page_url)
         yield scrapy.Request(absolute_next_page_url)
-
+        
